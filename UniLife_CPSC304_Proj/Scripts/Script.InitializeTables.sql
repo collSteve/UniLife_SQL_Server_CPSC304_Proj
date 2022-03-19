5ece41d3f8aa89e020cd214ca32785fd97962c1d
Ke Ren <rkglimmer16@gmail.com>
2022-03-19 13:00:33 -0700

    Add dummy data for file type and posts

--- UniLife_CPSC304_Proj/Scripts/Script.InitializeTables.sql
+++ UniLife_CPSC304_Proj/Scripts/Script.InitializeTables.sql
@@ -42,7 +42,20 @@ Begin
 INSERT [dbo].[File]([FID], [File_Location], [File_Type], PID) 
 VALUES (1, 'https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__340.jpg', 'Image', ); 
 End
+
+If Not Exists(select * from [dbo].[File] where [FID]=2)
+Begin
+INSERT [dbo].[File]([FID], [File_Location], [File_Type], PID) 
+VALUES (2, 'https://www.gettyimages.ca/gi-resources/images/500px/983794168.jpg', 'Image', ); 
+End
+
+If Not Exists(select * from [dbo].[File] where [FID]=3)
+Begin
+INSERT [dbo].[File]([FID], [File_Location], [File_Type], PID) 
+VALUES (3, 'https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg', 'Image', ); 
+End
 */
+
 
 /* Steve */
 
@@ -64,8 +77,40 @@ INSERT [dbo].[User_Account]([AID], [Seller_Rating])
 VALUES (2, 9.8); 
 End
 
+
+/* Post */
 If Not Exists(select * from [dbo].Post where [PID]=1)
 Begin
 INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
-VALUES (1, CAST(N'2022-12-03' AS Date), 'My Post Title', 'My Post body', 5, 2, 2); 
+VALUES (1, CAST(N'2022-12-03' AS Date), 'Sell Pepper', 'I want to sell my pepper', 5, 2, 2); 
+End
+
+If Not Exists(select * from [dbo].Post where [PID]=2)
+Begin
+INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
+VALUES (2, CAST(N'1999-02-21' AS Date), 'Dogs are awesome', 'Dogs are humans best friends', 78, 0, 2); 
+End
+
+If Not Exists(select * from [dbo].Post where [PID]=3)
+Begin
+INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
+VALUES (3, CAST(N'2002-06-12' AS Date), 'Cats suck', 'Cats suck because their poops smells too bad', 910, 234, 2); 
+End
+
+If Not Exists(select * from [dbo].Post where [PID]=4)
+Begin
+INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
+VALUES (4, CAST(N'2002-06-12' AS Date), 'Eating Subway in Subway', 'Subs yeet eifjweifie', 9120, 234, 2); 
+End
+
+If Not Exists(select * from [dbo].Post where [PID]=5)
+Begin
+INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
+VALUES (5, CAST(N'2005-11-13' AS Date), 'Sales on textbooks', 'Moving out so selling my CS texts: <software is bad>: $12', 5, 2, 2); 
+End
+
+If Not Exists(select * from [dbo].Post where [PID]=6)
+Begin
+INSERT [dbo].Post([PID], [Create_Date], [Title], [Post_Body], [Num_Likes], [Num_Dislikes], [Creator_UID]) 
+VALUES (6, CAST(N'1997-10-13' AS Date), 'Selling My Table tennis Brackets', 'Pro tb brackets for only 100 bucks', 5, 2, 2); 
 End
